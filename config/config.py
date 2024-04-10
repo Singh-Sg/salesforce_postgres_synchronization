321765d4b13555d4017a55b7560d886cf45f424a
developer <developer@gmail.com>
2024-04-10 12:50:50 +0530

    config: added configurations for salesforce sync

---
+++ config/config.py
@@ -0,0 +1,22 @@
+# config/config.py
+import logging
+
+class Config:
+    SALESFORCE_USERNAME = 'your-username-password'
+    SALESFORCE_PASSWORD = 'your-salesforce-password'
+    SALESFORCE_SECURITY_TOKEN = 'your-salesforce-sercurity-token'
+    SALESFORCE_CLIENT_ID = 'your-salesforce-client-id'
+    SALESFORCE_CLIENT_SECRET = 'salesforce-client-secret'
+
+    POSTGRES_HOST = 'localhost'
+    POSTGRES_PORT = '5432'
+    POSTGRES_DB = 'sfdb'
+    POSTGRES_USER = 'postgres'
+    POSTGRES_PASSWORD = 'root'
+
+    REDIS_HOST = 'your_redis_host'
+    REDIS_PORT = 'your_redis_port'
+    REDIS_PASSWORD = 'your_redis_password'
+
+    LOG_FILE = 'salesforce_postgres_sync.log'
+    LOG_LEVEL = logging.INFO
